f6ab66f1dbdc348db707913614529d3e77931e61
abhigkar <abhinav.golwalkar@gmail.com>
2025-10-04 22:32:36 +0530

    fix5

--- custom_components/tasmota_ir_hub/__init__.py
+++ custom_components/tasmota_ir_hub/__init__.py
@@ -24,10 +24,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     }
 
     # forward platforms
-    for platform in PLATFORMS:
-        hass.async_create_task(
-            hass.config_entries.async_forward_entry_setup(entry, platform)
-        )
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
     # Register services
     async def async_handle_send_ir(service_call):
